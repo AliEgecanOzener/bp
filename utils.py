2ad538d0a770fa6543d2cf0d48af5a26306b3bfb
Ali Egecan Özener <94221528+AliEgecanOzener@users.noreply.github.com>
2025-06-18 22:53:15 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
+import time
 import socket
 import random
 import string
 import requests
 import validators
+import cloudscraper
 from termcolor import colored
+from urllib.parse import quote, urlparse, urlunparse
 
 def url_validation_check(url):
     if validators.url(url):
@@ -58,7 +61,9 @@ def reformat_url(url):
         return url
 
 
-def get_query(url, cookie, header):
+
+def get_query(url, cookie, header, retries=5, delay=5):
+    session = cloudscraper.create_scraper()
 
     if isinstance(cookie, str):
         cookie = convert_string_to_dict(cookie) or {}
@@ -66,29 +71,31 @@ def get_query(url, cookie, header):
     if isinstance(header, str):
         header = convert_string_to_dict(header) or {}
 
-    try:
-        session = requests.Session()
-        response = session.get(url, cookies=cookie, headers=header, timeout=10)
-        return response
-
-    except requests.exceptions.Timeout:
+    for attempt in range(retries):
+        try:
+            response = session.get(url, cookies=cookie, headers=header, timeout=20)
+            return response
+        except requests.exceptions.ReadTimeout:
+            print(colored(f"[-] Read timeout. Retry {attempt + 1}/{retries}", "yellow"))
+            time.sleep(delay)
+        except requests.exceptions.Timeout:
             print(colored("[-] Request timed out.", "red"))
             raise
-    except requests.exceptions.ConnectionError:
+        except requests.exceptions.ConnectionError:
             print(colored("[-] Connection error occurred.", "red"))
             raise
-
-    except requests.exceptions.HTTPError as e:
+        except requests.exceptions.HTTPError as e:
             print(colored(f"[-] HTTP error: {e}", "red"))
             raise
-
-    except requests.exceptions.RequestException as e:
+        except requests.exceptions.RequestException as e:
             print(colored(f"[-] Unexpected error: {e}", "red"))
             raise
 
+    print(colored("[-] Failed after retries.", "red"))
+    return None
+
 
 def post_query(url, cookie, header, data, file):
-
     if isinstance(cookie, str):
         cookie = convert_string_to_dict(cookie) or {}
 
@@ -96,7 +103,7 @@ def post_query(url, cookie, header, data, file):
         header = convert_string_to_dict(header) or {}
 
     try:
-        session = requests.Session()
+        session = cloudscraper.create_scraper()
 
         if file:
             response = session.post(url, cookies=cookie, headers=header, data=data, files=file, timeout=10)
@@ -131,9 +138,9 @@ def get_random_user_agent():
     try:
         with open("dir/random_agents", "r", encoding="utf-8") as file:
             user_agents = [line.strip() for line in file if line.strip()]
-        return convert_string_to_dict(random.choice(user_agents)) if user_agents else None
+        return random.choice(user_agents).strip() if user_agents else None
     except FileNotFoundError:
-        print(colored("[-] File Not found.","red"))
+        print(colored("[-] File Not found.", "red"))
         return None
 
 
@@ -165,4 +172,80 @@ def get_port():
         else:
             print(colored("[!] Invalid port number. Please enter a valid port between 1 and 65535.", "red"))
 
+def get_ip():
+    while True:
+        ip_input = input(colored("\n[*] Please enter the IP address to connect to: ", "yellow")).strip()
+        validators.ipv4(ip_input)
+        return ip_input
 
+
+def parse_comma(text):
+    parameters = []
+    if text:
+       text = text.split(",")
+    else:
+        return None
+
+    for t in text:
+        t = t.strip()
+        parameters.append(t)
+    if parameters:
+       return parameters
+    else:
+        return None
+
+def lowered(text):
+    if text:
+       for t in text:
+           t = t.lower()
+       return text
+    else:
+        return None
+
+
+def dict_to_string(data):
+    return '&'.join(f"{key}={value}" for key, value in data.items())
+
+
+def double_url_encode(s):
+    return quote(quote(s))
+
+def parse_headers(header_string):
+    headers = {}
+    for item in header_string.split(","):
+        if "=" not in item:
+            continue
+        key, value = item.split("=", 1)
+        headers[key.strip()] = value.strip()
+    return headers
+
+
+def http_basic_auth(url, creds):
+    parsed = urlparse(url)
+
+    if not parsed.netloc or ":" not in creds:
+        return url, None
+
+    host = parsed.hostname
+    port = f":{parsed.port}" if parsed.port else ""
+    auth_netloc = f"{creds}@{host}{port}"
+
+    new_url = urlunparse((
+        parsed.scheme,
+        auth_netloc,
+        parsed.path,
+        parsed.params,
+        parsed.query,
+        parsed.fragment
+    ))
+
+    headers = {"User-Agent": get_random_user_agent()}
+    try:
+        resp = requests.get(new_url, headers=headers, timeout=10)
+        set_cookie = resp.headers.get('Set-Cookie')
+        return new_url, set_cookie
+    except:
+        return new_url, None
+
+
+http_basic_auth("http://10.10.10.18/WebGoat/attack?Screen=150&menu=400","guest:guest")
